51f73dc5ba9d3c915b8ca6daa00b434d50f0ff9f
Ramza2547 <Ram0908793988@gmail.com>
2026-03-22 19:33:29 +0700

    เพิ่ม localhost เข้าไปใน CORS

--- backend/settings.py
+++ backend/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://8mansions.vercel.app",
     "http://localhost:5173",
